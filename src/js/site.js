16d01e210b29d489b2ab7a0abd9db71f099bfa49
Taylor Radtke <taylorradtke@gmail.com>
2018-04-18 19:30:05 -0400

    added opening hero and blockquote

--- src/js/site.js
+++ src/js/site.js
@@ -1,1 +1,35 @@
+// BgClip Headings
+(function($) {
+  let s;
+  let BgClip = {
+    settings: {
+      heading: $('.js-bg-clip'),
+    },
+    init() {
+      s = this.settings;
+      this.bindEvents();
+    },
+    bindEvents() {
+      $(window).on('load resize scroll', $.proxy(this.getBgClip, this));
+    },
 
+    getBgClip() {
+      s.heading.each(function() {
+        const layerOffset = $(this)
+          .closest('article, section')
+          .offset();
+        const containerOffset = layerOffset.top - $(window).scrollTop();
+        BgClip =
+          containerOffset -
+          $(this)
+            .css('top')
+            .replace(/[^-\d\.]/g, '') -
+          $(this)
+            .css('margin-top')
+            .replace(/[^-\d\.]/g, '');
+        $(this).css('clip', `rect(${BgClip}px, auto, auto, auto)`);
+      });
+    },
+  };
+  BgClip.init();
+})(jQuery);
