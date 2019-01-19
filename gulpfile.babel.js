a242274d7e07838e81eb6950f897a15542c2fce0
Taylor Radtke <taylorradtke@gmail.com>
2019-01-19 13:06:31 -0500

    fixed potential security vulnerabilities in dependencies

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,97 +1,157 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync');
-const sourcemaps = require('gulp-sourcemaps');
-const sass = require('gulp-sass');
-const babel = require('gulp-babel');
-const autoprefixer = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
-const changed = require('gulp-changed');
-const del = require('del');
-const sequence = require('run-sequence');
-const htmlMin = require('gulp-htmlmin');
-const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
+import gulp from "gulp";
+import autoprefixer from "autoprefixer";
+import browsersync from "browser-sync";
+import cssnano from "cssnano";
+import del from "del";
+import eslint from "gulp-eslint";
+import htmlmin from "gulp-htmlmin";
+import imagemin from "gulp-imagemin";
+import newer from "gulp-newer";
+import plumber from "gulp-plumber";
+import postcss from "gulp-postcss";
+import rename from "gulp-rename";
+import sass from "gulp-sass";
+import webpack from "webpack";
+import webpackstream from "webpack-stream";
+import babel from "gulp-babel";
+import sassGlob from "gulp-sass-glob";
+import sourcemaps from "gulp-sourcemaps";
+import webpackconfig from "./webpack.config";
 
-gulp.task('reload', () => {
-  browserSync.reload();
-});
+browsersync.create();
 
-gulp.task('serve', ['js', 'sass'], () => {
-  browserSync({
-    server: 'src/',
-  });
-  gulp.watch('src/*.html', ['reload']);
-  gulp.watch('src/js/**/*.js', ['js']);
-  gulp.watch('src/scss/**/*.scss', ['sass']);
-});
+// project
+const project = { root: __dirname };
+project.dist = `${project.root}/docs`;
 
-gulp.task('sass', () =>
-  gulp
-    .src('src/scss/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(
-      autoprefixer({
-        browsers: ['last 3 versions'],
-      })
-    )
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('src/css/'))
-    .pipe(browserSync.stream())
+project.assets = `${project.root}/src`;
+project.img = `${project.assets}/img`;
+project.scss = `${project.assets}/scss`;
+project.js = `${project.assets}/js`;
+project.fonts = `${project.assets}/fonts`;
+project.favicon = `${project.assets}/favicon`;
+
+// BrowserSync
+function browserSync(done) {
+	browsersync.init({
+		server: {
+			baseDir: `${project.dist}/`
+		},
+		port: 3000
+	});
+	done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+	browsersync.reload();
+	done();
+}
+
+// Clean assets
+function clean() {
+	return del([`${project.dist}`]);
+}
+
+// Copy Favicon Assets
+function favicon() {
+	return gulp.src(`${project.favicon}/**/*`).pipe(gulp.dest(`${project.dist}`));
+}
+
+// Minify HTML
+function html() {
+	return gulp
+		.src(`${project.assets}/index.html`)
+		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(gulp.dest(`${project.dist}`));
+}
+
+// Copy Fonts Assets
+function fonts() {
+	return gulp
+		.src(`${project.fonts}/**/*`)
+		.pipe(gulp.dest(`${project.dist}/fonts/`));
+}
+
+// Optimize Images
+function images() {
+	return gulp
+		.src(`${project.img}/**/*`)
+		.pipe(newer(`${project.dist}/img/`))
+		.pipe(
+			imagemin([
+				imagemin.gifsicle({ interlaced: true }),
+				imagemin.jpegtran({ progressive: true }),
+				imagemin.optipng({ optimizationLevel: 5 }),
+				imagemin.svgo({
+					plugins: [
+						{
+							removeViewBox: false,
+							collapseGroups: true
+						}
+					]
+				})
+			])
+		)
+		.pipe(gulp.dest(`${project.dist}/img/`));
+}
+
+// CSS task
+function css() {
+	return gulp
+		.src(`${project.scss}/**/*.scss`)
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(sassGlob())
+		.pipe(sass({ outputStyle: "expanded" }))
+		.pipe(gulp.dest(`${project.dist}/css/`))
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(gulp.dest(`${project.dist}/css/`))
+		.pipe(browsersync.stream());
+}
+
+// Lint scripts
+function scriptsLint() {
+	return gulp
+		.src([`${project.js}/**/*`, "./gulpfile.babel.js"])
+		.pipe(plumber())
+		.pipe(eslint())
+		.pipe(eslint.format());
+	// .pipe(eslint.failAfterError());
+}
+
+// Transpile, concatenate and minify scripts
+function scripts() {
+	return (
+		gulp
+			.src([`${project.js}/**/*`])
+			.pipe(babel())
+			.pipe(plumber())
+			.pipe(webpackstream(webpackconfig, webpack))
+			// folder only, filename is specified in webpack config
+			.pipe(gulp.dest(`${project.dist}/js/`))
+			.pipe(browsersync.stream())
+	);
+}
+
+// Watch files
+function watchFiles() {
+	gulp.watch(`${project.assets}/*.html`, html);
+	gulp.watch(`${project.scss}/**/*`, css);
+	gulp.watch(`${project.js}/**/*`, gulp.series(scriptsLint, scripts));
+	gulp.watch(`${project.img}/**/*`, images);
+}
+
+// define complex tasks
+const js = gulp.series(scriptsLint, scripts);
+const build = gulp.series(
+	clean,
+	gulp.parallel(html, css, favicon, images, js, fonts)
 );
+const watch = gulp.parallel(watchFiles, browserSync);
 
-gulp.task('js', () =>
-  gulp
-    .src('src/js/site.js')
-    .pipe(sourcemaps.init())
-    .pipe(
-      babel({
-        presets: ['env'],
-      })
-    )
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('src/js/dist/'))
-);
+// export tasks
+export { html, images, favicon, fonts, css, js, clean, build, watch };
 
-gulp.task('js-compress', () =>
-  gulp
-    .src('src/js/dist/site.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('docs/js'))
-);
-
-gulp.task('img', () =>
-  gulp
-    .src('src/assets/**/*.{jpg,jpeg,png,gif}')
-    .pipe(changed('docs/assets/'))
-    .pipe(imagemin())
-    .pipe(gulp.dest('docs/assets/'))
-);
-
-gulp.task('css', () =>
-  gulp
-    .src('src/css/*.css')
-    .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(gulp.dest('docs/css'))
-);
-
-gulp.task('html', () =>
-  gulp
-    .src('src/*.html')
-    .pipe(
-      htmlMin({
-        sortAttributes: true,
-        sortClassName: true,
-        collapseWhitespace: true,
-      })
-    )
-    .pipe(gulp.dest('docs/'))
-);
-
-gulp.task('clean', () => del(['docs/']));
-
-gulp.task('build', () => {
-  sequence('clean', ['html', 'js', 'img', 'sass', 'css', 'js-compress']);
-});
-
-gulp.task('default', ['serve']);
+export default build;
--- src/js/site.js
+++ src/js/site.js
@@ -1,156 +1,161 @@
 // BgClip Headings
 (function($) {
-  let s;
-  let BgClip = {
-    settings: {
-      heading: $('.js-bg-clip'),
-    },
-    init() {
-      s = this.settings;
-      this.bindEvents();
-    },
-    bindEvents() {
-      $(window).on('load resize scroll', $.proxy(this.getBgClip, this));
-    },
+	let s;
+	let BgClip = {
+		settings: {
+			heading: $(".js-bg-clip")
+		},
+		init() {
+			s = this.settings;
+			this.bindEvents();
+		},
+		bindEvents() {
+			$(window).on("load resize scroll", $.proxy(this.getBgClip, this));
+		},
 
-    getBgClip() {
-      s.heading.each(function() {
-        const layerOffset = $(this)
-          .closest('article, section')
-          .offset();
-        const containerOffset = layerOffset.top - $(window).scrollTop();
-        BgClip =
-          containerOffset -
-          $(this)
-            .css('top')
-            .replace(/[^-\d\.]/g, '') -
-          $(this)
-            .css('margin-top')
-            .replace(/[^-\d\.]/g, '');
-        $(this).css('clip', `rect(${BgClip}px, auto, auto, auto)`);
-      });
-    },
-  };
-  BgClip.init();
+		getBgClip() {
+			s.heading.each(function() {
+				const layerOffset = $(this)
+					.closest("article, section")
+					.offset();
+				const containerOffset = layerOffset.top - $(window).scrollTop();
+				BgClip =
+					containerOffset -
+					$(this)
+						.css("top")
+						.replace(/[^-\d\.]/g, "") -
+					$(this)
+						.css("margin-top")
+						.replace(/[^-\d\.]/g, "");
+				$(this).css("clip", `rect(${BgClip}px, auto, auto, auto)`);
+			});
+		}
+	};
+	BgClip.init();
 })(jQuery);
 
 /* ---------------------------------------------
   Height 100%
 --------------------------------------------- */
-function js_height_init() {
-  (function($) {
-    $('.js-height-full').height($(window).height());
-    $('.js-height-parent').each(function() {
-      $(this).height(
-        $(this)
-          .parent()
-          .first()
-          .height()
-      );
-    });
-  })(jQuery);
+function jsHeightInit() {
+	(function($) {
+		$(".js-height-full").height($(window).height());
+		$(".js-height-parent").each(function() {
+			$(this).height(
+				$(this)
+					.parent()
+					.first()
+					.height()
+			);
+		});
+	})(jQuery);
 }
 
 /* ---------------------------------------------
   Scroll navigation
 --------------------------------------------- */
 
-function init_scroll_navigate() {
-  $('a.scrollLink').click(function(event) {
-    event.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500);
-  });
+function initScrollNavigate() {
+	$("a.scrollLink").click(function(event) {
+		event.preventDefault();
+		$("html, body").animate(
+			{ scrollTop: $($(this).attr("href")).offset().top },
+			500
+		);
+	});
 }
 
 /* ---------------------------------------------
   Scroll Spy
 --------------------------------------------- */
 
-function init_scroll_spy() {
-  // Cache selectors
-  let lastId;
-  const topMenu = $('#top-menu');
-  const topMenuHeight = topMenu.outerHeight() + 15;
-  // All list items
-  const menuItems = topMenu.find('a');
-  // Anchors corresponding to menu items
-  const scrollItems = menuItems.map(function() {
-    const item = $($(this).attr('href'));
-    if (item.length) {
-      return item;
-    }
-  });
+function initScrollSpy() {
+	// Cache selectors
+	let lastId;
+	const topMenu = $("#top-menu");
+	const topMenuHeight = topMenu.outerHeight() + 15;
+	// All list items
+	const menuItems = topMenu.find("a");
+	// Anchors corresponding to menu items
+	const scrollItems = menuItems.map(function() {
+		const item = $($(this).attr("href"));
+		if (item.length) {
+			return item;
+		}
+	});
 
-  // Bind click handler to menu items
-  // so we can get a fancy scroll animation
-  menuItems.click(function(e) {
-    let href = $(this).attr('href'),
-      offsetTop = href === '#' ? 0 : $(href).offset().top - topMenuHeight + 1;
-    $('html, body')
-      .stop()
-      .animate(
-        {
-          scrollTop: offsetTop,
-        },
-        300
-      );
-    e.preventDefault();
-  });
+	// Bind click handler to menu items
+	// so we can get a fancy scroll animation
+	menuItems.click(function(e) {
+		const href = $(this).attr("href");
 
-  // Bind to scroll
-  $(window).scroll(function() {
-    // Get container scroll position
-    const fromTop = $(this).scrollTop() + topMenuHeight;
+		const offsetTop =
+			href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+		$("html, body")
+			.stop()
+			.animate(
+				{
+					scrollTop: offsetTop
+				},
+				300
+			);
+		e.preventDefault();
+	});
 
-    // Get id of current scroll item
-    let cur = scrollItems.map(function() {
-      if ($(this).offset().top < fromTop) return this;
-    });
-    // Get the id of the current element
-    cur = cur[cur.length - 1];
-    const id = cur && cur.length ? cur[0].id : '';
+	// Bind to scroll
+	$(window).scroll(function() {
+		// Get container scroll position
+		const fromTop = $(this).scrollTop() + topMenuHeight;
 
-    if (lastId !== id) {
-      lastId = id;
-      // Set/remove active class
-      menuItems
-        .parent()
-        .removeClass('active')
-        .end()
-        .filter(`[href='#${id}']`)
-        .parent()
-        .addClass('active');
-    }
-  });
+		// Get id of current scroll item
+		let cur = scrollItems.map(function() {
+			if ($(this).offset().top < fromTop) return this;
+		});
+		// Get the id of the current element
+		cur = cur[cur.length - 1];
+		const id = cur && cur.length ? cur[0].id : "";
+
+		if (lastId !== id) {
+			lastId = id;
+			// Set/remove active class
+			menuItems
+				.parent()
+				.removeClass("active")
+				.end()
+				.filter(`[href='#${id}']`)
+				.parent()
+				.addClass("active");
+		}
+	});
 }
 
 // Navbar
-function init_nav_show() {
-  $(window).scroll(() => {
-    if ($(window).scrollTop() > 220) {
-      $('.site-header').removeClass('site-header__show');
-    } else {
-      $('.site-header').addClass('site-header__show');
-    }
-  });
+function initNavShow() {
+	$(window).scroll(() => {
+		if ($(window).scrollTop() > 220) {
+			$(".site-header").removeClass("site-header__show");
+		} else {
+			$(".site-header").addClass("site-header__show");
+		}
+	});
 }
 /* ---------------------------------------------
      Scripts initialization
      --------------------------------------------- */
 
 $(window).load(() => {
-  $(window).trigger('scroll');
-  $(window).trigger('resize');
+	$(window).trigger("scroll");
+	$(window).trigger("resize");
 });
 
 $(document).ready(() => {
-  init_scroll_navigate();
-  init_scroll_spy();
-  init_nav_show();
-  new WOW().init();
-  $(window).trigger('resize');
+	initScrollNavigate();
+	initScrollSpy();
+	initNavShow();
+	new WOW().init();
+	$(window).trigger("resize");
 });
 
 $(window).resize(() => {
-  js_height_init();
+	jsHeightInit();
 });
